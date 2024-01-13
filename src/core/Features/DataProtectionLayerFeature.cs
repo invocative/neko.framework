c40fb61a5598ea941238184ab80fd6f170447184
Yuuki Wesp <ls-micro@ya.ru>
2024-01-13 11:29:22 +0300

    update

--- src/core/Features/DataProtectionLayerFeature.cs
+++ src/core/Features/DataProtectionLayerFeature.cs
@@ -5,6 +5,7 @@ using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 [PublicAPI]
 public record DataProtectionLayerFeature(Action<IDataProtectionBuilder> Config) : AppFeature
@@ -32,10 +33,58 @@ public record StaticFilesFeature(StaticFileOptions Config) : AppFeature
 [PublicAPI]
 public static class StaticFilesFeatureIAppBuilder
 {
-    public static IAppBuilder StaticFiles(this IAppBuilder builder, Action<StaticFileOptions> config)
-        => builder.InjectFeature(() => {
+    public static IAppBuilder StaticFiles(this IAppBuilder builder, Action<StaticFileOptions, IAppCreationContext> config)
+        => builder.InjectFeature((x) => {
             var opt = new StaticFileOptions();
-            config(opt);
+            config(opt, x);
             return new StaticFilesFeature(opt);
         });
+}
+
+
+[PublicAPI]
+public record ResponseCachingFeature : AppFeature
+{
+    public override void BeforeRun(WebApplication webBuilder)
+        => webBuilder.UseResponseCaching();
+
+    public override void BeforeRegistrations(WebApplicationBuilder webBuilder) 
+        => webBuilder.Services.AddResponseCaching();
+}
+
+[PublicAPI]
+public static class ResponseCachingFeatureIAppBuilder
+{
+    public static IAppBuilder ResponseCaching(this IAppBuilder builder)
+        => builder.InjectFeature(() => new ResponseCachingFeature());
+}
+
+
+[PublicAPI]
+public record DevelopmentFeature(bool IsEnabled) : AppFeature
+{
+    public override void BeforeRun(WebApplication app)
+    {
+        if (!IsEnabled)
+            return;
+        app.UseExceptionHandler();
+        app.UseStatusCodePages();
+        app.UseDeveloperExceptionPage();
+    }
+    public override void BeforeRegistrations(WebApplicationBuilder webBuilder)
+    {
+        if (!IsEnabled)
+            return;
+        webBuilder.Services.AddProblemDetails();
+    }
+}
+
+[PublicAPI]
+public static class DevelopmentFeatureIAppBuilder
+{
+    public static IAppBuilder Development(this IAppBuilder builder, bool enabled)
+        => builder.InjectFeature(() => new DevelopmentFeature(enabled));
+
+    public static IAppBuilder Development(this IAppBuilder builder)
+        => builder.InjectFeature(x => new DevelopmentFeature(x.Environment.IsDevelopment()));
 }
